4bdd442513490d79b37c3c516544c980fcf41911
BrunoGilRamirez <bruno.gilram@gmail.com>
2025-06-17 13:30:00 -0600

    first commit

---
+++ src/main/java/com/aspiresys/fp_micro_userservice/FpMicroUserserviceApplication.java
@@ -0,0 +1,13 @@
+package com.aspiresys.fp_micro_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpMicroUserserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpMicroUserserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aspiresys/fp_micro_userservice/FpMicroUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspiresys.fp_micro_userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpMicroUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
